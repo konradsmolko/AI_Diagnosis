221966c2cac221ed74cc1a587214833bfae61bba
konrad <17689319+konradsmolko@users.noreply.github.com>
2019-06-02 00:15:36 +0200

    seems to be working...

--- KRKPA7/code/main.py
+++ KRKPA7/code/main.py
@@ -1,13 +1,17 @@
+from random import Random
+
 from KRKPA7.code.dataIO import read_data
 from KRKPA7.code.ai_logic import decisionTree, test, predict
 
 
 def main():
-    data = read_data('../data/arrhythmia.data')
-    tree = decisionTree(data)
-    tree.print()
+    data = read_data('../data/kr-vs-kp.data')
+    x = Random(160698)
+    x.shuffle(data, x.random)
+    tree = decisionTree(data[:2000])
+    # tree.print()
     print(tree.depth())
-    print(test(data, tree))
+    print(test(data[2000:], tree))
 
 
 if __name__ == '__main__':
