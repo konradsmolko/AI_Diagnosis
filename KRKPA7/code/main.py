29f50258fee94d507033a8084a77cc90484bd293
konrad <17689319+konradsmolko@users.noreply.github.com>
2019-06-04 03:33:36 +0200

    done

--- KRKPA7/code/main.py
+++ KRKPA7/code/main.py
@@ -6,12 +6,26 @@ from KRKPA7.code.ai_logic import decisionTree, test, predict
 
 def main():
     data = read_data('../data/kr-vs-kp.data')
-    x = Random(160698)
-    x.shuffle(data, x.random)
-    tree = decisionTree(data[:2000])
-    # tree.print()
-    print(tree.depth())
-    print(test(data[2000:], tree))
+    labels = ['bkblk', 'bknwy', 'bkon8', 'bkona', 'bkspr', 'bkxbq', 'bkxcr', 'bkxwp', 'blxwp', 'bxqsq', 'cntxt',
+              'dsopp', 'dwipd', 'hdchk', 'katri', 'mulch', 'qxmsq', 'r2ar8', 'reskd', 'reskr', 'rimmx', 'rkxwp',
+              'rxmsq', 'simpl', 'skach', 'skewr', 'skrxp', 'spcop', 'stlmt', 'thrsk', 'wkcti', 'wkna8', 'wknck',
+              'wkovl', 'wkpos', 'wtoeg', 'CLASS']
+    rand = Random(160698)
+    results = []
+    tries = 1000
+    for _ in range(tries):
+        rand.shuffle(data, rand.random)
+        split_point = rand.randint(2000, 2500)
+        tree = decisionTree(data[:split_point])
+        while tree.depth() >= 20 or tree.depth() < 6:
+            rand.shuffle(data, rand.random)
+            split_point = rand.randint(2000, 2500)
+            tree = decisionTree(data[:split_point])
+        results.append(test(data[split_point:], tree))
+        # tree.print()
+        # print("Splitpoint:", split_point, "Depth:", tree.depth(), "Accuracy:", results[-1])
+
+    print("Average accuracy over", tries, "tries:", sum(results)/tries)
 
 
 if __name__ == '__main__':
